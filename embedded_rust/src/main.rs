d61fdb36cd55039f16a7d8985356b1002daa5c54
Craig <ceaglebu@purdue.edu>
2025-06-08 15:11:03 -0400

    spi working

--- embedded_rust/src/main.rs
+++ embedded_rust/src/main.rs
@@ -4,7 +4,7 @@
 use cortex_m::asm::nop;
 use panic_halt as _;
 use cortex_m_rt::entry;
-use stm32f4xx_hal::{self as hal, gpio::GpioExt, hal::spi, spi::SpiExt};
+use stm32f4xx_hal::{self as hal, gpio::GpioExt, hal::spi, rcc::RccExt, spi::SpiExt, time::Hertz};
 use hal::pac;
 // use rtt_target::{rtt_init_print, rprintln};
 #[entry]
@@ -15,14 +15,39 @@ fn main() -> ! {
     // GPIO A initialization
     let p = pac::Peripherals::take().unwrap();
     let gpioa = p.GPIOA.split();
-    let mut pa5 = gpioa.pa5.into_push_pull_output();
+    let d_cx = gpioa.pa5.into_push_pull_output(); // data / command select
+    let mut cs = gpioa.pa4.into_push_pull_output();
+
+    let gpiob = p.GPIOB.split();
+    let sck = gpiob.pb3.into_alternate();
+    let miso = gpiob.pb4.into_alternate();
+    let mosi = gpiob.pb5.into_alternate();
+
+    let rcc = p.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze();
+    // rprintln!("{}", clocks.pclk1().to_Hz());
+
+    let spi = p.SPI1.spi(
+        (sck, miso, mosi), 
+        spi::Mode {
+            polarity: spi::Polarity::IdleLow, 
+            phase: spi::Phase::CaptureOnFirstTransition
+        }, 
+        Hertz::MHz(8), 
+        &clocks
+    );
+
+    // 8 bit data frame is default
+    // MSB first is default
+
+    let mut spi = spi.init();
+
+    cs.set_low();
+    spi.write(&[0xDE, 0xAD, 0xBE, 0xEF]).unwrap();
 
     loop {
-        match pa5.is_set_high() {
-            true => pa5.set_low(),
-            false => pa5.set_high(),
-        }
-        for _ in 0..100_000 {
+        for _ in 0..1_000_000 {
             nop();
         }
     }
